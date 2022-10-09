201f21c723487572afc46f5607935d70377b66fc
JTByland <jeffrey.byland@utdallas.edu>
2022-10-09 12:49:21 -0500

    Started forum page

---
+++ src/components/GroupCard.js
@@ -0,0 +1,12 @@
+const GroupCard = ({
+    groupname,
+}) => {
+    
+    return (
+    <div class="card m-2 p-5" className="GroupCard">
+        <h1>{ groupname }</h1>
+    </div>
+    );
+}
+     
+export default GroupCard;
