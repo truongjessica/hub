6b957234139c751dd8f278732e0ce49f9bb869ca
lleavuong <93018471+lleavuong@users.noreply.github.com>
2022-10-09 18:21:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/NavbarComponent';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/NavbarComponent";
+import { FlashCard, Group, Home, VideoCall, Forum, Error, JoinTeam, CreateTeam, Register, Login } from "./pages";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import GroupSelectionPage from "./pages/GroupSelectionPage";
 
 function App() {
   return (
-    <div className="App">
-      <NavbarComponent/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element=<SharedLayout />>
+          <Route index element=<Home />></Route>
+          <Route path="/flash-cards" element=<FlashCard /> />
+          <Route path="/forum" element=<Forum /> />
+          <Route path="/group" element=<GroupSelectionPage /> />
+          <Route path="/join-team" element=<JoinTeam /> />
+          <Route path="/create-team" element=<CreateTeam /> />
+          <Route path="/meeting" element=<VideoCall /> />
+          <Route path="/register" element=<Register /> />
+          <Route path="/login" element=<Login /> />
+          <Route path="*" element=<Error /> />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
