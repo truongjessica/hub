a33fbd60ad26d06446a5aac291fe9df092e3d318
truongjessica <truongjessica@sbcglobal.net>
2022-10-09 14:07:15 -0500

    add join team page

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,7 +1,39 @@
-import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Group = () => {
-  return <div>Group</div>;
+  return (
+    <div>
+      <Card
+        style={{
+          width: "90%",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Card.Header>Join Team</Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Group Name</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Label><b>OR</b></Form.Label>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Group ID</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Send Request
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Group;
