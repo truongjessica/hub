10cb1b5c2ddb198c371be9eede5cfe3f0b271826
JTByland <jeffrey.byland@utdallas.edu>
2022-10-25 18:39:28 -0500

    Updated Forum page styling

--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,10 +1,21 @@
 const GroupCard = ({
     groupname,
+    selected,
 }) => {
-    
+    const pressed ={
+        boxShadow: "inset 0 0 10px #aeaeae",
+        borderRadius: '10px',
+        background: '#fafafa'
+    }
+
+    const unPressed ={
+        boxShadow: "0 0 10px #e0e0e0",
+        borderRadius: '10px'
+    }
+
     return (
-    <div class="card m-2 p-5">
-        <h1>{ groupname }</h1>
+    <div class="m-2 p-5" style={ selected ? pressed : unPressed }>
+        <div style={ selected ? {fontSize:'20px', color:'orange'} : {fontSize:'20px', color:'grey'} }>{ groupname }</div>
     </div>
     );
 }
