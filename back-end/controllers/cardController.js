c7e911572a2bc6bd15e1207cec41472ec68549b7
nguyen tran <nguyen.tran6698@gmail.com>
2022-11-08 01:04:12 -0600

    Currently working on

--- back-end/controllers/cardController.js
+++ back-end/controllers/cardController.js
@@ -2,6 +2,7 @@ const Card = require("../models/Flashcard");
 const Group = require("../models/Group");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const User = require("../models/User");
 
 const getAllCards = async (req, res) => {
   const { user } = req;
@@ -11,17 +12,19 @@ const getAllCards = async (req, res) => {
 };
 
 const createCard = async (req, res) => {
-  const { title, question, answer } = req.body;
+  const { group_name, title, question, answer } = req.body;
   const { userId } = req.user;
-  const group = await Group.find({ user: userId });
-
+  const group = await Group.find({ user: userId, name: group_name });
+  if (!group) {
+    throw new CustomError.BadRequestError(`Group does not exist `);
+  }
   const newCard = { title, question, answer, group: group._id };
   const card = await Card.create(newCard);
   res.status(StatusCodes.CREATED).json({ card });
 };
 
 const getSingleCard = async (req, res) => {
-  const card = await Flashcard.findOne({ title: req.params.title });
+  const card = await Card.findOne({ title: req.params.title });
   if (!card) {
     throw new CustomError.NotFoundError(
       `No card with title : ${req.params.title}`
@@ -31,18 +34,17 @@ const getSingleCard = async (req, res) => {
 };
 
 const updateCard = async (req, res) => {
-  const { title, question, answer, user } = req.body;
-  if (!title || !question || !answer || !user) {
-    throw new CustomError.BadRequestError("Please provide all values");
+  const { userId } = req.user;
+  const { id } = req.params;
+  const card = await Card.findById({ _id: id });
+  const user = await User.findById({
+    _id: userId,
+    groups: { $in: card.group._id },
+  });
+  if (!user) {
+    throw new CustomError.UnauthorizedError(`Can't modified this card`);
   }
-  const card = await Flashcard.findOne({ title: req.params.title });
-
-  card.title = title;
-  card.question = question;
-  card.answer = answer;
-
-  await card.save();
-  res.status(StatusCodes.OK).send("Update Card");
+  res.status(StatusCodes.OK).json({ msg: "update done" });
 };
 
 const removeCard = async (req, res) => {
